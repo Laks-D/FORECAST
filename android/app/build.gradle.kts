59945a03eb7ecafc60e37de746350078e3a8319b
Rohith Thayalan <rohiththayalan@MacBookPro.lan>
2026-06-15 19:45:45 +0530

    chore: remove dev/prod flavors to simplify build process

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,19 +34,6 @@ android {
         versionName = flutter.versionName
     }
 
-    flavorDimensions += "env"
-
-    productFlavors {
-        create("dev") {
-            dimension = "env"
-            applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev"
-        }
-        create("prod") {
-            dimension = "env"
-        }
-    }
-
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
